7236be2fdd531675bdfa9b61d9850db034f22cab
Constantin NB <sainquake@gmail.com>
2018-05-20 15:11:47 +0300

    canard

---
+++ Src/uavcan.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2017 Konstantin Burdinov <sainquake@gmail.com>
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file uavcan.c
+ * @author sainquake
+ */
+
+#include "canard.h"
+#include "canard_stm32.h"
+
+void uavcan_init()
+{
+
+}

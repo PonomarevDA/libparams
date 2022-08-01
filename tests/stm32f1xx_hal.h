6bea872baa3b0fd912fa5f870dc0a236361f81c1
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-08-01 18:20:51 +0300

    move arm build test to his own folder

--- tests/stm32f1xx_hal.h
+++ tests/stm32f1xx_hal.h
@@ -10,6 +10,8 @@
 #define FLASH_TYPEERASE_PAGES     0x00U
 #define FLASH_TYPEPROGRAM_WORD    0x02U
 
+#include <stdint.h>
+
 typedef enum
 {
   HAL_OK       = 0x00U,
@@ -26,9 +28,10 @@ typedef struct
   uint32_t NbPages;
 } FLASH_EraseInitTypeDef;
 
-HAL_StatusTypeDef HAL_FLASH_Unlock(void) {return HAL_OK;}
-HAL_StatusTypeDef HAL_FLASH_Lock(void) {return HAL_OK;}
-HAL_StatusTypeDef HAL_FLASHEx_Erase(FLASH_EraseInitTypeDef *pEraseInit, uint32_t *PageError) {return HAL_OK;}
-HAL_StatusTypeDef HAL_FLASH_Program(uint32_t TypeProgram, uint32_t Address, uint64_t Data) {return HAL_OK;}
+void HAL_FLASH_Init(uint32_t initial_addr);
+HAL_StatusTypeDef HAL_FLASH_Unlock(void);
+HAL_StatusTypeDef HAL_FLASH_Lock(void);
+HAL_StatusTypeDef HAL_FLASHEx_Erase(FLASH_EraseInitTypeDef *pEraseInit, uint32_t *PageError);
+HAL_StatusTypeDef HAL_FLASH_Program(uint32_t TypeProgram, uint32_t Address, uint64_t Data);
 
 #endif  // STM32F1XX_HAL_MOCK_H_

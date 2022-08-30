a20e8acb82a6adbca0d0fd53ecce138c933295ee
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-08-31 00:15:32 +0300

    refactor generator script in OOP style a little bit

--- scripts/params_generate_array.py
+++ scripts/params_generate_array.py
@@ -6,17 +6,39 @@ from glob import glob
 LANGUAGE_C = 0
 LANGUAGE_CPP = 1
 
-INPUT_PARAM_FILE_NAME = ["*_params.c", "*_params.cpp"]
-GENERATED_SOURCE_FILE_NAME = ["params.c", "params.cpp"]
-GENERATED_HEADER_FILE_NAME = ["params.h", "params.hpp"]
-GENERATED_HPP_FILE_NAME = "params.hpp"
-START_SOURCE_LINE = "IntegerDesc_t integer_desc_pool[] __attribute__((unused)) = {"
-END_HEADER_LINE = ["} IntParamsIndexes;", "};"]
-INCLUDE_STORAGE_LINE = ["#include \"storage.h\"\n\n", "extern \"C\" {\n    #include \"storage.h\"\n}\n\n"]
 
-END_SOURCE_LINE = "};"
-START_HEADER_LINE = ["typedef enum {", "enum class IntParamsIndexes {"]
+class AbstractGenerator:
+    def __init__(self) -> None:
+        self.input_param_file_name = ""
+        self.generated_source_file_name = ""
+        self.generated_header_file_name = ""
+        self.generated_hpp_file_name = "params.hpp"
+        self.start_source_line = "IntegerDesc_t integer_desc_pool[] __attribute__((unused)) = {"
+        self.end_header_line = ""
+        self.include_storage_line = "#include \"storage.h\"\n\n"
+        self.end_source_line = "};"
+        self.array_line = "IntegerParamValue_t integer_values_pool[sizeof(integer_desc_pool) / sizeof(IntegerDesc_t)];\n"
+        self.start_header_line = ""
 
+class CGenerator(AbstractGenerator):
+    def __init__(self) -> None:
+        super().__init__()
+        self.input_param_file_name = "*_params.c"
+        self.generated_source_file_name = "params.c"
+        self.generated_header_file_name = "params.h"
+        self.end_header_line = "} IntParamsIndexes;"
+        self.include_storage_line = "extern \"C\" {\n    #include \"storage.h\"\n}\n\n"
+        self.start_header_line = "typedef enum {"
+
+class CppGenerator(AbstractGenerator):
+    def __init__(self) -> None:
+        super().__init__()
+        self.input_param_file_name = "*_params.cpp"
+        self.generated_source_file_name = "params.cpp"
+        self.generated_header_file_name = "params.hpp"
+        self.end_header_line = "};"
+        self.include_storage_line = ""
+        self.start_header_line = "enum class IntParamsIndexes {"
 
 def print_help():
     usage_line = "python3 params_generate_array.py [input_dir] [output_dir] [language: `c++` or `c`]"
@@ -78,24 +100,24 @@ def generate(input_files, out_fd, start_line, end_line):
             for line_idx in range(first_param_idx, last_param_idx + 1):
                 out_fd.write(lines[line_idx])
 
-def generate_source_files(input_files, output_file, language):
-    out_fd = open(output_file + GENERATED_SOURCE_FILE_NAME[language], 'w')
-    out_fd.write(f"#include \"{GENERATED_HEADER_FILE_NAME[language]}\"\n")
-    out_fd.write(INCLUDE_STORAGE_LINE[language])
+def generate_source_files(input_files, output_file, generator):
+    out_fd = open(output_file + generator.generated_source_file_name, 'w')
+    out_fd.write(f"#include \"{generator.generated_header_file_name}\"\n")
+    out_fd.write(generator.include_storage_line)
 
-    out_fd.write(f"{START_SOURCE_LINE}\n")
-    generate(input_files, out_fd, START_SOURCE_LINE, END_SOURCE_LINE)
-    out_fd.write(f"{END_SOURCE_LINE}\n")
-    out_fd.write("IntegerParamValue_t integer_values_pool[sizeof(integer_desc_pool) / sizeof(IntegerDesc_t)];\n")
+    out_fd.write(f"{generator.start_source_line}\n")
+    generate(input_files, out_fd, generator.start_source_line, generator.end_source_line)
+    out_fd.write(f"{generator.end_source_line}\n")
+    out_fd.write(generator.array_line)
     out_fd.close()
 
-def generate_header_files(input_files, output_file, language):
-    out_fd = open(output_file+GENERATED_HEADER_FILE_NAME[language], 'w')
+def generate_header_files(input_files, output_file, generator):
+    out_fd = open(output_file+generator.generated_header_file_name, 'w')
     out_fd.write("#pragma once\n\n")
-    out_fd.write(f"{START_HEADER_LINE[language]}\n")
-    generate(input_files, out_fd, START_HEADER_LINE[language], END_HEADER_LINE[language])
+    out_fd.write(f"{generator.start_header_line}\n")
+    generate(input_files, out_fd, generator.start_header_line, generator.end_header_line)
     out_fd.write("    INTEGER_PARAMS_AMOUNT\n")
-    out_fd.write(f"{END_HEADER_LINE[language]}\n")
+    out_fd.write(f"{generator.end_header_line}\n")
     out_fd.close()
 
 def sort_files_by_priorities(files):
@@ -106,13 +128,13 @@ def sort_files_by_priorities(files):
             break
     return files
 
-def find_src_files(input_file, language):
+def find_src_files(input_file, generator):
     config_fd = open(input_file, 'r')
     source_files_paths = config_fd.readlines()
     all_src_files = []
     for line in source_files_paths:
         source_files_path = line.rstrip()
-        src_files = [y for x in os.walk(source_files_path) for y in glob(os.path.join(x[0], INPUT_PARAM_FILE_NAME[language]))]
+        src_files = [y for x in os.walk(source_files_path) for y in glob(os.path.join(x[0], generator.input_param_file_name))]
         if len(src_files) != 0:
             all_src_files.append(*src_files)
     config_fd.close()
@@ -122,7 +144,14 @@ def find_src_files(input_file, language):
 
 if __name__=="__main__":
     input_dir, output_dir, language = parse_args()
-    src_files = find_src_files(input_dir, language)
 
-    generate_source_files(src_files, output_dir, language)
-    generate_header_files(src_files, output_dir, language)
+    if language == LANGUAGE_C:
+        generator = CGenerator()
+    elif language == LANGUAGE_CPP:
+        generator = CppGenerator()
+    else:
+        exit()
+
+    src_files = find_src_files(input_dir, generator)
+    generate_source_files(src_files, output_dir, generator)
+    generate_header_files(src_files, output_dir, generator)

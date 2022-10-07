0026ab35d8ea75e3800d5bb5ca54d614bfb8294b
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-10-08 00:53:13 +0300

    fix cpp issue of generator script

--- scripts/params_generate_array.py
+++ scripts/params_generate_array.py
@@ -36,7 +36,6 @@ class ParamsGenerator:
     def write_beginning_header_part(self, output_file):
         out_header_fd = open(output_file+generator.generated_header_file_name, 'w')
         out_header_fd.write("#pragma once\n")
-        out_header_fd.write("#include \"storage.h\"\n")
         out_header_fd.write("\n")
         out_header_fd.write(f"{self.start_hdr_line}\n")
         return out_header_fd

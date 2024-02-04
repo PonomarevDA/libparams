12a153daf4985bb70dd41552f5958048454d2bde
PonomarevDA <ponomarevda96@gmail.com>
2024-02-05 00:56:17 +0300

    clean up paramters generator with pylint

--- scripts/params.py
+++ scripts/params.py
@@ -6,29 +6,53 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+"""Integer and String parameters."""
+
+import sys
 from dataclasses import dataclass
 from color_logging import log_warn, log_err
 
 @dataclass
-class IntegerParam:
+class BaseParam:
     name: str = ""
+    default: int = 0
+    note: str = ""
+    mutability: str = "MUTABLE"
+
+@dataclass
+class IntegerParam(BaseParam):
+    """
+    Suitable for:
+    - Cyphal    uavcan.register.Value.1.0.integer32
+    - DroneCAN  uavcan.param.Value.integer_value
+    """
     enum_name: str = ""
     flags: str = ""
-    default: int = 0
     min: int = 0
     max: int = 0
-    mutability: str = "MUTABLE"
     is_required: str = "false"
 
     @staticmethod
     def create(param_name, data):
-        if type(data) is dict:
+        if isinstance(data, dict):
             integer_parameter = IntegerParam._create_from_dict(param_name, data)
-        elif type(data) is list:
+        elif isinstance(data, list):
             integer_parameter = IntegerParam._create_from_list_legacy(param_name, data)
         else:
             integer_parameter = None
         return integer_parameter
+
+    @staticmethod
+    def create_port_id(param_name, enum_base : str):
+        id_register = IntegerParam(
+            name=f"\"{param_name}.id\"",
+            flags="",
+            enum_name=f"{enum_base}_ID",
+            default=65535,
+            min=0,
+            max=65535,
+        )
+        return id_register
 
     @staticmethod
     def _create_from_dict(param_name, data : dict):
@@ -65,39 +89,37 @@ class IntegerParam:
         )
         return integer_parameter
 
-    @staticmethod
-    def create_cyphal_port_id(param_name, enum_base : str):
-        id_register = IntegerParam(
-            name=f"\"{param_name}.id\"",
-            flags="",
-            enum_name=f"{enum_base}_ID",
-            default=65535,
-            min=0,
-            max=65535,
-        )
-        return id_register
-
 @dataclass
-class StringParam:
-    name: str = ""
-    default: str = ""
-    mutability: str = "IMMUTABLE"
-
+class StringParam(BaseParam):
+    """
+    Suitable for:
+    - Cyphal    uavcan.register.Value.1.0.string
+    - DroneCAN  uavcan.param.Value.string_value
+    """
     @staticmethod
     def create(param_name, data):
-        if type(data) is dict:
+        if isinstance(data, dict):
             string_param = StringParam._create_from_dict(param_name, data)
-        elif type(data) is list:
+        elif isinstance(data, list):
             string_param = StringParam._create_from_list_legacy(param_name, data)
         else:
             string_param = None
         return string_param
 
     @staticmethod
+    def create_port_type(param_name, data_type : str):
+        type_register = StringParam(
+            name=f"\"{param_name}.type\"",
+            default=f"\"{data_type}\"",
+            mutability="IMMUTABLE"
+        )
+        return type_register
+
+    @staticmethod
     def _create_from_dict(param_name, data : dict):
         string_param = StringParam(
             name=f"\"{param_name}\"",
-            default="\"{}\"".format(data['default']),
+            default=f"\"{data['default']}\"",
             mutability=is_mutable(param_name, str(data['flags']))
         )
         if 'note' in data:
@@ -111,30 +133,25 @@ class StringParam:
         """Input example: param_name : ["data_type",  "ENUM_NAME"]"""
         string_param = StringParam(
             name=f"\"{param_name}\"",
-            default="\"{}\"".format(data[3]),
+            default=f"\"{data[3]}\"",
             mutability=is_mutable(param_name, str(data[2]))
         )
         return string_param
 
-    @staticmethod
-    def create_cyphal_port_type(param_name, data_type : str):
-        type_register = StringParam(
-            name=f"\"{param_name}.type\"",
-            default=f"\"{data_type}\"",
-            mutability="IMMUTABLE"
-        )
-        return type_register
-
 def is_mutable(param_name : str, mutability_str : str):
+    assert isinstance(param_name, str)
+    assert isinstance(mutability_str, str)
     name = f"\"{param_name}\""
     if mutability_str in ["True", "immutable"]:
         mutability = "IMMUTABLE"
     elif mutability_str in ["False", "mutable"]:
         mutability = "MUTABLE"
     else:
-        log_err(f"Mutability of {name} can't be determined: {mutability_str} ({type(mutability_str)})")
-        exit()
+        log_err((f"Mutability of {name} can't be determined: "
+                 f"{mutability_str} ({type(mutability_str)})"))
+        sys.exit()
     if mutability_str in ["True", "False"]:
-        log_warn(f"Mutability of {name} will be deprecated soon: {mutability_str}. Use `mutable` or `immutable`.")
+        log_warn((f"Mutability of {name} will be deprecated soon: "
+                  f"{mutability_str}. Use `mutable` or `immutable`."))
 
     return mutability

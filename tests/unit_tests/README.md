1b816c9cbe2f9f76f24f98734fc74e1d533d86ba
PonomarevDA <ponomarevda96@gmail.com>
2024-07-07 21:41:20 +0300

    improve ROM driver unit tests, now it is 100%

--- tests/unit_tests/README.md
+++ tests/unit_tests/README.md
@@ -16,3 +16,28 @@ make
 in this folder.
 
 This tests are part of [the build CI](.github/workflows/build.yml).
+
+# ROM driver
+
+## Test Case 1: Initialize ROM Driver Instance
+
+- Test 1.1: Initialize with Single Latest Page, Negative Number
+- Test 1.2: Initialize with Single Latest Page, Positive Number
+- Test 1.3: Initialize with Multiple Pages
+- Test 1.4: Initialize with Invalid Page Index
+- Test 1.5: Initialize with Zero Pages
+
+### Test Case 2: Read from ROM
+
+- Test 2.1: Read Data within Bounds
+- Test 2.2: Read Data Exceeding Bounds
+
+### Test Case 3: Write to ROM
+
+- Test 3.1: Write Data within Bounds
+- Test 3.2: Write Data Exceeding Bounds
+
+### Test Case 4: Get Available Memory
+
+- Test 4.1: Verify Available Memory Calculation
+- Test 4.2: Verify Available Memory Calculation with nullptr
--- tests/unit_tests/test_rom.cpp
+++ tests/unit_tests/test_rom.cpp
@@ -12,93 +12,124 @@
 #include "libparams_error_codes.h"
 #include "flash_driver.h"
 
-// RomInit normal usage
-TEST(TestRom, test_romInit_normal_first_page) {
-    RomDriverInstance rom = romInit(0, 1);
+// Test Case 1. Initialize ROM Driver Instance
+// Test 1.1: Initialize with Single Latest Page, Negative Number
+TEST(TestRom, test_1_1_initialize_with_single_latest_page_negative_number) {
+    auto rom = romInit(-1, 1);
     ASSERT_TRUE(rom.inited);
 }
-TEST(TestRom, test_romInit_normal_last_page) {
-    RomDriverInstance rom = romInit(flashGetNumberOfPages() - 1, 1);
+// Test 1.2: Initialize with Single Latest Page, Positive Number
+TEST(TestRom, test_1_2_initialize_with_single_latest_page_positive_number) {
+    auto rom = romInit(flashGetNumberOfPages() - 1, 1);
     ASSERT_TRUE(rom.inited);
 }
-TEST(TestRom, test_romInit_normal_all_pages) {
-    RomDriverInstance rom = romInit(0, flashGetNumberOfPages());
+// Test 1.3: Initialize with Multiple Pages
+TEST(TestRom, test_1_3_initialize_with_multiple_pages) {
+    auto rom = romInit(0, flashGetNumberOfPages());
     ASSERT_TRUE(rom.inited);
 }
-
-// RomInit bad arguments
-TEST(TestRom, test_romInit_fail_zero_pages) {
-    RomDriverInstance rom = romInit(0, 0);
+// Test 1.4: Initialize with Invalid Page Index
+TEST(TestRom, test_1_4_initialize_with_invalid_page_index) {
+    auto rom = romInit(flashGetNumberOfPages(), 1);
     ASSERT_FALSE(rom.inited);
 }
-TEST(TestRom, test_romInit_fail_out_of_memory) {
-    RomDriverInstance rom = romInit(flashGetNumberOfPages(), 1);
+// Test 1.5: Initialize with Zero Pages
+TEST(TestRom, test_1_5_initialize_with_zero_pages) {
+    auto rom = romInit(-1, 0);
     ASSERT_FALSE(rom.inited);
 }
 
 
-// romRead normal usage
-TEST(TestRom, test_romRead_normal) {
-    RomDriverInstance rom = romInit(0, 1);
-    uint8_t data[4048];
+// Test Case 2: Read from ROM
+// Test 2.1: Read Data within Bounds
+TEST(TestRom, test_2_1_read_data_withing_bounds) {
+    auto rom = romInit(0, flashGetNumberOfPages());
+    const auto ROM_SIZE = romGetAvailableMemory(&rom);
+    uint8_t data[ROM_SIZE];
 
-    // Normal, full read
+    // Read a chank of data
     ASSERT_EQ(romRead(&rom, 0, data, 8), 8);
+
+    // Read everything
+    ASSERT_EQ(romRead(&rom, 0, data, ROM_SIZE), ROM_SIZE);
+}
+// Test 2.2: Read Data Exceeding Bounds
+TEST(TestRom, test_2_2_read_data_exceeding_bounds) {
+    auto rom = romInit(0, flashGetNumberOfPages());
+    const auto ROM_SIZE = romGetAvailableMemory(&rom);
+    uint8_t data[ROM_SIZE];
 
     // Normal, clamped read
-    ASSERT_EQ(romRead(&rom, 0, data, 4096), 2048);
-    ASSERT_EQ(romRead(&rom, 1000, data, 4096), 1048);
-    ASSERT_EQ(romRead(&rom, 1000, data, 2048), 1048);
-    ASSERT_EQ(romRead(&rom, 1000, data, 2000), 1048);
-}
-
-// romRead bad arguments
-TEST(TestRom, test_romRead_bad_arguments) {
-    RomDriverInstance rom = romInit(0, 1);
-    uint8_t data[4048];
+    ASSERT_EQ(romRead(&rom, 0, data, ROM_SIZE + 1), ROM_SIZE);
+    ASSERT_EQ(romRead(&rom, 1, data, ROM_SIZE), ROM_SIZE - 1);
 
     // Wrong inputs
-    ASSERT_EQ(romRead(&rom, 0, NULL, 8), 0);
-    ASSERT_EQ(romRead(&rom, 4096, data, 8), 0);
+    ASSERT_EQ(romRead(nullptr, 0, data, ROM_SIZE), 0);
+    ASSERT_EQ(romRead(&rom, 0, nullptr, ROM_SIZE), 0);
+    ASSERT_EQ(romRead(&rom, ROM_SIZE, data, ROM_SIZE), 0);
     ASSERT_EQ(romRead(&rom, 0, data, 0), 0);
-
-    // Normal, full read
-    ASSERT_EQ(romRead(&rom, 0, data, 8), 8);
 }
 
+// Test Case 3: Write to ROM
+// Test 3.1: Write Data within Bounds
+TEST(TestRom, test_3_1_write_data_withing_bounds) {
+    auto rom = romInit(0, flashGetNumberOfPages());
+    const uint8_t SAMPLE_DATA[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    uint8_t read_data[sizeof(SAMPLE_DATA)] = {};
 
-// romWrite normal usage
-TEST(TestRom, test_romWrite_normal) {
-    RomDriverInstance rom = romInit(0, 1);
-    const uint8_t WRITTEN_DATA[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    ASSERT_EQ(rom.total_size, flashGetPageSize());
-
-    // Normal inputs, but without romBeginWrite and romEndWrite()
-    ASSERT_EQ(romWrite(&rom, 0, WRITTEN_DATA, 8), 0);
-
-    // Normal
     romBeginWrite(&rom);
-    size_t write_res = romWrite(&rom, 0, WRITTEN_DATA, 8);
+    ASSERT_EQ(romWrite(&rom, 0, SAMPLE_DATA, sizeof(SAMPLE_DATA)), sizeof(SAMPLE_DATA));
     romEndWrite(&rom);
-    ASSERT_EQ(write_res, 8);
 
-    uint8_t read_data[8] = {};
-    size_t read_res = romRead(&rom, 0, read_data, 8);
-    ASSERT_EQ(read_res, 8);
-    int compare_res = memcmp(WRITTEN_DATA, read_data, 8);
-    ASSERT_EQ(compare_res, 0);
+    ASSERT_EQ(romRead(&rom, 0, read_data, sizeof(SAMPLE_DATA)), sizeof(SAMPLE_DATA));
+    ASSERT_EQ(memcmp(SAMPLE_DATA, read_data, sizeof(SAMPLE_DATA)), 0);
 }
-// romWrite bad arguments
-TEST(TestRom, test_romWrite_bad_args) {
-    RomDriverInstance rom = romInit(0, 1);
-    const uint8_t WRITTEN_DATA[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    ASSERT_EQ(rom.total_size, flashGetPageSize());
+// Test 3.2: Write Data Exceeding Bounds
+TEST(TestRom, test_3_2_romWrite_bad_args) {
+    auto rom = romInit(0, flashGetNumberOfPages());
+    const uint8_t SAMPLE_DATA[] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-    // Wrong inputs
-    ASSERT_EQ(romWrite(&rom, 0, NULL, 8), 0);
-    ASSERT_EQ(romWrite(&rom, 4096, WRITTEN_DATA, 8), 0);
-    ASSERT_EQ(romWrite(&rom, 0, WRITTEN_DATA, 0), 0);
-    ASSERT_EQ(romWrite(&rom, 1500, WRITTEN_DATA, 1500), 0);
+    // 1. ROM nullptr
+    romBeginWrite(&rom);
+    ASSERT_EQ(romWrite(nullptr, 0, SAMPLE_DATA, sizeof(SAMPLE_DATA)), 0);
+    romEndWrite(&rom);
+
+    // 2. Offset out of bound
+    ASSERT_EQ(romWrite(&rom, rom.total_size, SAMPLE_DATA, sizeof(SAMPLE_DATA)), 0);
+
+    // 3. Data nullptr
+    romBeginWrite(&rom);
+    ASSERT_EQ(romWrite(&rom, 0, nullptr, sizeof(SAMPLE_DATA)), 0);
+    romEndWrite(&rom);
+
+    // 4. Zero wring size
+    romBeginWrite(&rom);
+    ASSERT_EQ(romWrite(&rom, 0, SAMPLE_DATA, 0), 0);
+    romEndWrite(&rom);
+
+    // 5. Offset and size out of bound
+    romBeginWrite(&rom);
+    ASSERT_EQ(romWrite(&rom, 1500, SAMPLE_DATA, 1500), 0);
+    romEndWrite(&rom);
+
+    // 6. Without romBeginWrite() and romEndWrite()
+    ASSERT_EQ(romWrite(&rom, 0, SAMPLE_DATA, sizeof(SAMPLE_DATA)), 0);
+
+    // 7. romBeginWrite() and romEndWrite() nullptr
+    romBeginWrite(nullptr);
+    ASSERT_EQ(romWrite(&rom, 0, SAMPLE_DATA, sizeof(SAMPLE_DATA)), 0);
+    romEndWrite(nullptr);
+}
+
+// Test Case 4: Get Available Memory
+// Test 4.1: Verify Available Memory Calculation with correct_input
+TEST(TestRom, test_4_1_verify_avaliable_memory_calculation_with_correct_input) {
+    auto rom = romInit(0, flashGetNumberOfPages());
+    ASSERT_EQ(romGetAvailableMemory(&rom), flashGetNumberOfPages() * flashGetPageSize());
+}
+// Test 4.2: Verify Available Memory Calculation with nullptr
+TEST(TestRom, test_4_2_verify_avaliable_memory_calculation_with_nullptr) {
+    ASSERT_EQ(romGetAvailableMemory(nullptr), 0);
 }
 
 int main (int argc, char *argv[]) {

f60a917503c9061d19350a9afa8cf1c5ab8f564b
PonomarevDA <ponomarevda96@gmail.com>
2024-07-21 12:38:55 +0300

    finish astyle checker script

--- scripts/check_astyle.py
+++ scripts/check_astyle.py
@@ -11,12 +11,16 @@ Check code style with astyle
 import os
 import sys
 import logging
+import subprocess
 from typing import Optional
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-def get_source_files(directories: list, extensions=('.c', '.cpp', '.h', '.hpp')):
+def get_source_files(directories: list, extensions=('.c', '.cpp', '.h', '.hpp')) -> list:
+    assert isinstance(directories, list)
+    assert isinstance(extensions, tuple)
+
     source_files = []
     for directory in directories:
         for path in Path(directory).rglob('*'):
@@ -24,31 +28,88 @@ def get_source_files(directories: list, extensions=('.c', '.cpp', '.h', '.hpp'))
                 source_files.append(path)
     return source_files
 
-def main(directories: list, astylerc: Optional[str]):
+def astyle_single_file(origin_file_path: str,
+                       pretty_file_path: str,
+                       astylerc_path: Optional[str]=None) -> None:
+    astyle_check_cmd = ['astyle']
+    if astylerc_path is not None:
+        astyle_check_cmd.append(f"--options={astylerc_path}")
+
+    with open(origin_file_path, 'r', encoding='UTF-8') as infile:
+        logger.debug(" ".join(astyle_check_cmd))
+        result = subprocess.run(
+            astyle_check_cmd,
+            input=infile.read(),
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+    with open(pretty_file_path, 'w', encoding='UTF-8') as outfile:
+        outfile.write(result.stdout)
+
+def compare_two_files_wth_git_diff(first_file_path: str, second_file_path: str) -> str:
+    git_diff_cmd = [
+        'git',
+        '--no-pager',
+        'diff',
+        '--no-index',
+        '--minimal',
+        '--histogram',
+        '--color=always',
+        first_file_path,
+        second_file_path
+    ]
+
+    logger.debug(" ".join(git_diff_cmd))
+    result = subprocess.run(git_diff_cmd, capture_output=True, text=True, check=False)
+    return result.stdout
+
+
+def check_single_file(source_file_path: str, astylerc_path: Optional[str]=None) -> str:
+    assert isinstance(source_file_path, str)
+    assert isinstance(astylerc_path, str) or astylerc_path is None
+
+    temp_pretty_file = f"{source_file_path}.pretty"
+    astyle_single_file(source_file_path, temp_pretty_file, astylerc_path)
+    res = compare_two_files_wth_git_diff(str(source_file_path), temp_pretty_file)
+    os.remove(temp_pretty_file)
+
+    return res
+
+def main(directories: list, astylerc_path: Optional[str]):
     assert isinstance(directories, list)
-    assert isinstance(astylerc, str) or astylerc is None
+    assert isinstance(astylerc_path, str) or astylerc_path is None
 
-    for dir in directories:
-        if not os.path.exists(dir):
-            sys.exit(f"Error: the directory `{dir}` is not exist. Abort.")
+    for directory in directories:
+        if not os.path.exists(directory):
+            sys.exit(f"Error: the directory `{directory}` is not exist. Abort.")
 
-    if astylerc is not None and not os.path.exists(astylerc):
-        sys.exit(f"Error: astylerc path `{astylerc}` specified, but can't be found. Abort.")
+    if astylerc_path is not None and not os.path.exists(astylerc_path):
+        sys.exit(f"Error: astylerc path `{astylerc_path}` specified, but can't be found. Abort.")
 
-    logging.debug(f"Directories: {directories}")
+    logging.debug("Directories: %s", directories)
 
     files = get_source_files(directories)
+    res = ""
     for file in files:
-        logging.debug(file)
+        logging.debug("\n\n%s", file)
+        res += check_single_file(source_file_path=str(file), astylerc_path=astylerc_path)
+
+    if len(res) > 0:
+        print(res)
+        sys.exit("ERROR: Fix code style!")
 
 if __name__=="__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
 
     from argparse import ArgumentParser
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('directories', nargs='+', help='Directories to search for source files')
-    parser.add_argument("--astylerc", type=str, required=False, help="Optional path to astylerc file")
+    parser.add_argument('directories', nargs='+',
+                        help='Directories to search for source files')
+    parser.add_argument("--astylerc", type=str, required=False,
+                        help="Optional path to astylerc file")
 
     args = parser.parse_args()
     main(args.directories, args.astylerc)

1c70915c43cc5bd301d5116e4551afb5b15251e6
PonomarevDA <ponomarevda96@gmail.com>
2025-03-15 14:55:14 +0300

    update return types of rom driver

--- libparams/libparams_error_codes.h
+++ libparams/libparams_error_codes.h
@@ -9,10 +9,22 @@
 #ifndef LIBPARAMS_ERROR_CODES_H_
 #define LIBPARAMS_ERROR_CODES_H_
 
+// HAL related error codes [-1; -19]
 #define LIBPARAMS_OK                    0
 #define LIBPARAMS_WRONG_ARGS            -2
 #define LIBPARAMS_UNKNOWN_HAL_ERROR     -4
 #define LIBPARAMS_NOT_INITIALIZED       -8
+
+// Flash memory related error codes [-20; -39]
+
+// ROM related error codes [-40; -59]
+#define LIBPARAMS_ROM_WRITE_BAD_ARGS_ERROR  -40
+#define LIBPARAMS_ROM_WRITE_PROTECTED_ERROR -41
+#define LIBPARAMS_ROM_WRITE_UNKNOWN_ERROR   -59
+
+
+// Storage related errors [-60; -79]
+
 #define LIBPARAMS_UNKNOWN_ERROR         -128
 
 #endif  // LIBPARAMS_ERROR_CODES_H_

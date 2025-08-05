87fc3160f9bbab0aa9cd31b2f6287a05edc111b1
AsiiaPine <asiiapine@gmail.com>
2025-08-05 15:23:57 +0300

    upd copyright

--- scripts/params.py
+++ scripts/params.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # Copyright (c) 2023 Dmitry Ponomarev <ponomarevda96@gmail.com>
-#
+#                    Anastasiia Stepanova <asiiapine@gmail.com>
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
